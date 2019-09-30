8dc3640a3e626096dcbefef18dd2b6c348c8f6c5
Sai Prakash <saiprakash.avula.com>
2019-09-30 16:33:28 +0000

    security update done

--- providers.tf
+++ providers.tf
@@ -1,8 +1,6 @@
 provider "aws" {
     region = "us-west-2"
 #    region = "${var.region}"
-#    access_key = "${var.access_key}"
-#    secret_key = "${var.secret_key}"
-    access_key = "AKIATRVTTA6YCM6MNORE"
-    secret_key = "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"
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
 }
